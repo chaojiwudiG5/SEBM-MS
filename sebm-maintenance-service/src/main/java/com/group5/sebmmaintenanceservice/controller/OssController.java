a6ad4a94145d7651046b40390052e21ff1121235
布洛芬下蛋 <1144607840@qq.com>
2025-10-21 19:56:37 +0800

    fix bugs

--- sebm-maintenance-service/src/main/java/com/group5/sebmmaintenanceservice/controller/OssController.java
+++ sebm-maintenance-service/src/main/java/com/group5/sebmmaintenanceservice/controller/OssController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/oss")
+@RequestMapping("/oss")
 @Slf4j
 public class OssController {
 
